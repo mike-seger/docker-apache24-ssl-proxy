dd2b373ea328849a92ce7b3c9e55df19694946e2
michael <michael@n.c>
2020-11-09 08:36:44 +0000

    curl experiments

--- httpd/testws.sh
+++ httpd/testws.sh
@@ -6,12 +6,18 @@ port=$3
 path=$4
 echo $host:$port
 
+curl -i -N -H "Connection: Upgrade" -H "Upgrade: websocket" \
+	-H "Host: $host" \
+	-H "Origin: $protocol://$host:$port" \
+	$protocol://$host:$port/$path
+	
+exit 0	
 curl --include \
      --no-buffer \
      --header "Connection: Upgrade" \
      --header "Upgrade: websocket" \
      --header "Host: $host" \
-     --header "Origin: protocol://$host:$port/$path" \
+     --header "Origin: $protocol://$host:$port/$path" \
      --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
      --header "Sec-WebSocket-Version: 13" \
      $protocol://$host:$port/$path
